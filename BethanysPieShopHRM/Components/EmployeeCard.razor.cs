ea9617474e1b32cf8d9c214ecb1cde5f1934ceb1
Bean <beansobre@gmail.com>
2023-04-22 12:54:16 +0800

    Section First Chapter

---
+++ BethanysPieShopHRM/Components/EmployeeCard.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BethanysPieShopHRM.Components
+{
+    public partial class EmployeeCard
+    {
+        [Parameter]
+        public Employee Employee { get; set; } = default!;
+    }
+}
